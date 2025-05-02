ed0a1bd8d6fea1a692d181ff03346eb7f5f146ff
Jeevangovindaraju <govindarajujeevan@gmail.com>
2025-05-03 00:56:42 +0530

    fix: removed unused Image import

--- src/app/threads/components/HeroSection.tsx
+++ src/app/threads/components/HeroSection.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
+
 
 export default function HeroSection() {
   const audioRef = useRef<HTMLAudioElement>(null);
